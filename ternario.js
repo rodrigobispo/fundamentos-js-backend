f15c801671e65b45beea0006521b2fd6ee2320a1
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-26 04:36:46 -0300

    Indentação, teste...

--- ternario.js
+++ ternario.js
@@ -2,9 +2,9 @@ const idadeMinima = 18;
 const idadeCliente = 19;
 
 if (idadeCliente >= idadeMinima) {
-    console.log("Maior de idade.");
+  console.log("Maior de idade.");
 } else {
-    console.log("Menor de idade.");
+  console.log("Menor de idade.");
 }
 
 console.log(idadeCliente >= idadeMinima ? "Maior de idade." : "Menor de idade.")
