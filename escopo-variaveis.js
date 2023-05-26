5d0614be4592bc319d2f9d1f136a8fb568967d5e
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-26 11:57:31 -0300

    Indentação

--- escopo-variaveis.js
+++ escopo-variaveis.js
@@ -3,11 +3,10 @@ let idade = 29
 const pi = 3.14
 
 {
-    var respostaDeTudo = 3.14
-    let idade = 42
-    const pi = 29
-    console.log(respostaDeTudo, idade, pi) //resultado: 3.14 42 29
-
+  var respostaDeTudo = 3.14
+  let idade = 42
+  const pi = 29
+  console.log(respostaDeTudo, idade, pi) //resultado: 3.14 42 29
 }
 
 console.log(respostaDeTudo, idade, pi) //resultado: 3.14 29 3.14
