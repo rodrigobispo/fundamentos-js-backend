4ed31cfb704d97e016f942f3d8b3427bfacb0d60
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-06-07 09:53:52 -0300

    Mais alguns testes...

--- resumo-recursos-essenciais-shorthands/AtribuindoMultiplasVariaveis.js
+++ resumo-recursos-essenciais-shorthands/AtribuindoMultiplasVariaveis.js
@@ -16,15 +16,32 @@ let d, e, f;
 ({ d, e, f } = obj);
 console.log(d, e, f);
 
+
+console.log("Pessoa: ")
 const pessoa = {
   nome: "rodrigo",
   email: "rodrigo@email.com",
   telefone: '131313',
   idade: 42,
-  sexo: 'M'
+  sexo: 'M',
+  endereco: {
+    rua: "al xpto",
+    cep: "71000000",
+    cidade: "Brasilia"
+  }
 }
 const dados = {...pessoa};
+const { nome, email, sexo, idade } = {...pessoa};
 console.log(dados);
+console.log("Alguns dados da pessoa:", nome, email, sexo, idade); //rodrigo rodrigo@email.com M 42
+
+const { rua, cep, cidade } = {...pessoa};
+console.log("Dados do endereço:", cep, rua, cidade); //undefined undefined undefined
+
+const { endereco } = {...pessoa};
+console.log("Dados do endereço:", endereco); //{ rua: 'al xpto', cep: '71000000', cidade: 'Brasilia' }
+
+
 
 console.log("Espalhamento: ")
 function myFunction(v, w, x, y, z) {
@@ -32,3 +49,37 @@ function myFunction(v, w, x, y, z) {
 }
 var args = [0, 1];
 myFunction(-1, ...args, 2, ...[3]);
+
+
+
+const produto = {
+  "nome": "Cena do Crime",
+  "valor": 89.0,
+  "quantidade": 1200,
+  "descricao": "A primeira colaboração de Ed Brubaker e Michael Lark - antes de seus trabalhos aclamadíssimos.",
+  "caracteristicas": [{"nome": "Característica do Livro", "descricao": "Descrição"}],
+  "imagens": [{"url": "url", "descricao": "Descrição teste."}],
+  "categoria": "Livros/HQs",
+  "usuario_id": "1"
+}
+const data = {...produto}
+const { caracteristicas }= {...produto}
+const { imagens } = {...produto}
+console.log("Dados produto:", data);
+console.log("Dados caracteristicas:", caracteristicas);
+console.log("Dados imagens:", imagens);
+const produto2 = {
+  "nome": "Cena do Crime",
+  "valor": 89.0,
+  "quantidade": 1200,
+  "descricao": "A primeira colaboração de Ed Brubaker e Michael Lark - antes de seus trabalhos aclamadíssimos.",
+  "caracteristicas": [],
+  "imagens": [{"url": "url", "descricao": "Descrição teste."}],
+  "categoria": "Livros/HQs",
+  "usuario_id": "1"
+}
+const data2 = {...produto2}
+console.log("Dados produto2:", data2);
+console.log("Dados caracteristicas:", data2.caracteristicas);
+const objCaract = data2.caracteristicas
+console.log(...objCaract)
